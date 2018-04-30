b6335e0d77c8869200558c022b0f930149436a55
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-04-30 21:43:15 +0300

    initial

---
+++ Store/Goods.h
@@ -0,0 +1,15 @@
+//
+// Created by Taras Martynyuk on 4/30/2018.
+//
+
+#ifndef OOPFINALEXAM_GOODS_H
+#define OOPFINALEXAM_GOODS_H
+
+
+class Goods
+{
+
+};
+
+
+#endif //OOPFINALEXAM_GOODS_H
---
+++ cmake-build-debug/CMakeFiles/OOPFinalExam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOPFinalExam.dir/main.cpp.o"
+  "CMakeFiles/OOPFinalExam.dir/Store/Goods.cpp.o"
+  "OOPFinalExam.pdb"
+  "OOPFinalExam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOPFinalExam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
